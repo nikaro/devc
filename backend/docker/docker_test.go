5e44af5146ac10581ddabd4b1012b02539499fee
Nicolas KAROLAK <nicolas@karolak.fr>
2020-10-12 16:49:59 +0000

    test: add some basic tests

--- backend/docker/docker_test.go
+++ backend/docker/docker_test.go
@@ -1,1 +1,11 @@
 package docker
+
+import "testing"
+
+func TestGetImageName(t *testing.T) {
+	got := GetImageName("test_path")
+	want := "vsc-test_path-5da6ae5928d4a1ce395878ae9c7ea1f6"
+	if got != want {
+		t.Errorf("got: %s, want: %s", got, want)
+	}
+}
